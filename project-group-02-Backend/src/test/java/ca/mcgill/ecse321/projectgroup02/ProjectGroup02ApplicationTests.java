8035fb3d5d9a941c4f9e13242227a91d892cb98d
ryadammar1 <mohamed.ammar@mail.mcgill.ca>
2020-10-01 12:14:12 -0400

    Initial commit of the backend application

---
+++ project-group-02-Backend/src/test/java/ca/mcgill/ecse321/projectgroup02/ProjectGroup02ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.projectgroup02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGroup02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
