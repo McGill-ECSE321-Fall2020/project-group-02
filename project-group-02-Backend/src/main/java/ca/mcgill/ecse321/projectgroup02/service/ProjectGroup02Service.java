2947f5374d2571ecf455fee9522fe90cc2cbcd6d
ryadammar1 <mohamed.ammar@mail.mcgill.ca>
2020-10-20 16:36:39 -0400

    paymentcredentials

--- project-group-02-Backend/src/main/java/ca/mcgill/ecse321/projectgroup02/service/ProjectGroup02Service.java
+++ project-group-02-Backend/src/main/java/ca/mcgill/ecse321/projectgroup02/service/ProjectGroup02Service.java
@@ -1,4 +1,5 @@
 package ca.mcgill.ecse321.projectgroup02.service;
+
 import java.util.HashSet;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,131 +22,168 @@ import ca.mcgill.ecse321.projectgroup02.model.UserRole;
 
 @Service
 public class ProjectGroup02Service {
-    @Autowired
-    private ArtGallerySystemRepository artGallerySystemRepository;
-    @Autowired
-    private ApplicationUserRepository applicationUserRepository;
-    @Autowired
-    private AddressRepository addressRepository;
-    @Autowired
-    private ArtistRepository artistRepository;
-    @Autowired
-    private ItemRepository itemRepository;
-    @Autowired
-    private CollectionRepository collectionRepository;
-    @Autowired
-    private CustomerRepository customerRepository;
-    @Autowired
-    private ItemOrderRepository itemOrderRepository;
-    @Autowired
-    private PaymentCredentialsRepository paymentCredentialsRepository;
-    @Autowired
-    private ServiceProviderRepository serviceProviderRepository;
-    @Autowired
-    private ShoppingCartRepository shoppingCartRepository;
-    
-    /**
-     * @param username
-     * @param email
-     * @param password
-     * @return Instance of ApplicationUser if the method successfully created the user
-     * @throws Exception 
-     */
-    @Transactional
-    public ApplicationUser createUser(String username, String email, String password) throws Exception {
-        
-        try {
-          validateRegistrationInput(username, email, password);
-        }catch(Exception e) {
-          throw new Exception(e.getMessage());
-        }
-      
-        ApplicationUser user = new ApplicationUser();
-        Iterable<ApplicationUser> users = applicationUserRepository.findAll();
-        for (ApplicationUser user_ : users) {
-          if (user_.getEmail().equals(email) || user_.getUsername().equals(username)) {
-            throw new Exception("Username/Email already in use");
-          }
-        }
-        user.setUsername(username);
-        user.setEmail(email);
-        user.setPassword(password);
-        user.setAccountCreationDate(java.time.LocalDate.now().toString());
-        applicationUserRepository.save(user);
-        return user;
-    }
-    
-    private void validateRegistrationInput(String username, String email, String password) throws Exception {
-        if (username.length() < 8)
-          throw new Exception("Username must have at least 8 characters");
-        if (!email.contains("@"))
-          throw new Exception("Email is not valid");
-        if (password.length() < 8)
-          throw new Exception("Password must have at least 8 characters");
-    }
-    
-    @Transactional
-    public ApplicationUser getUser(String username) {
-        return applicationUserRepository.findByUsername(username);
-    }
-    
-    @Transactional
-    public Iterable<ApplicationUser> getAllUsers() {
-        return (applicationUserRepository.findAll());
-    }
-    
-    @Transactional
-    public ApplicationUser updateUserCredentials(String username, 
-            String cardHolderName, String ccNumber, String expirationDate, String cvc) throws Exception {
-        ApplicationUser user = applicationUserRepository.findByUsername(username);
-        Customer customer = null;
-        
-        for (UserRole role : user.getUserRole()) {
-          if (role instanceof Customer)
-            customer = (Customer) role;
-        }
-        
-        if (customer == null) 
-          throw new Exception("User must be a customer");
-        
-        PaymentCredentials paymentCredentials = new PaymentCredentials();
-        paymentCredentials.setCardHolderName(cardHolderName);
-        paymentCredentials.setCcNumber(ccNumber);
-        paymentCredentials.setCvc(cvc);
-        paymentCredentials.setExpirationDate(expirationDate);
-        
-        customer.getPaymentCredentials().add(paymentCredentials);
+  @Autowired
+  private ArtGallerySystemRepository artGallerySystemRepository;
+  @Autowired
+  private ApplicationUserRepository applicationUserRepository;
+  @Autowired
+  private AddressRepository addressRepository;
+  @Autowired
+  private ArtistRepository artistRepository;
+  @Autowired
+  private ItemRepository itemRepository;
+  @Autowired
+  private CollectionRepository collectionRepository;
+  @Autowired
+  private CustomerRepository customerRepository;
+  @Autowired
+  private ItemOrderRepository itemOrderRepository;
+  @Autowired
+  private PaymentCredentialsRepository paymentCredentialsRepository;
+  @Autowired
+  private ServiceProviderRepository serviceProviderRepository;
+  @Autowired
+  private ShoppingCartRepository shoppingCartRepository;
 
-        paymentCredentialsRepository.save(paymentCredentials);
-        customerRepository.save(customer);
-        applicationUserRepository.save(user);
-        return user;
+  /**
+   * Registers user based on information inputed. Verifies the validity of the information inputed.
+   * 
+   * @param username
+   * @param email
+   * @param password
+   * @throws Exception
+   * @author ryadammar1
+   */
+  @Transactional
+  public ApplicationUser createUser(String username, String email, String password) throws Exception {
+
+    try {
+      validateRegistrationInput(username, email, password);
+    } catch (Exception e) {
+      throw new Exception(e.getMessage());
     }
-    
-    @Transactional
-    public ApplicationUser updateUserRole(String username, 
-            HashSet<UserRole> role) {
-        ApplicationUser user = applicationUserRepository.findByUsername(username);
-        
-        user.setUserRole(role);
-        applicationUserRepository.save(user);
-        return user;
+
+    ApplicationUser user = new ApplicationUser();
+    Iterable<ApplicationUser> users = applicationUserRepository.findAll();
+    for (ApplicationUser user_ : users) {
+      if (user_.getEmail().equals(email) || user_.getUsername().equals(username)) {
+        throw new Exception("Username/Email already in use");
+      }
     }
-    
-    @Transactional
-    public void logoutUser(String username) {
-        ApplicationUser user = applicationUserRepository.findByUsername(username);
-        user.setIsLoggedIn(false);
+    user.setUsername(username);
+    user.setEmail(email);
+    user.setPassword(password);
+    user.setAccountCreationDate(java.time.LocalDate.now().toString());
+    applicationUserRepository.save(user);
+    return user;
+  }
+
+  /**
+   * Throws an exception for any user input that is not valid during registration.
+   * 
+   * @param username
+   * @param email
+   * @param password
+   * @throws Exception
+   * @author ryadammar1
+   */
+  private void validateRegistrationInput(String username, String email, String password) throws Exception {
+    if (username.length() < 8)
+      throw new Exception("Username must have at least 8 characters");
+    if (!email.contains("@"))
+      throw new Exception("Email is not valid");
+    if (password.length() < 8)
+      throw new Exception("Password must have at least 8 characters");
+  }
+
+  @Transactional
+  public ApplicationUser getUser(String username) {
+    return applicationUserRepository.findByUsername(username);
+  }
+
+  @Transactional
+  public Iterable<ApplicationUser> getAllUsers() {
+    return (applicationUserRepository.findAll());
+  }
+
+  /**
+   * Adds payment credentials information to a user. Verifies that the user is a customer.
+   * 
+   * @param username
+   * @param cardHolderName
+   * @param ccNumber
+   * @param expirationDate
+   * @param cvc
+   * @throws Exception
+   * @author ryadammar1
+   */
+  @Transactional
+  public ApplicationUser updateUserCredentials(String username, String cardHolderName, String ccNumber,
+      String expirationDate, String cvc) throws Exception {
+    ApplicationUser user = applicationUserRepository.findByUsername(username);
+    Customer customer = null;
+
+    for (UserRole role : user.getUserRole()) {
+      if (role instanceof Customer)
+        customer = (Customer) role;
     }
-    
-    @Transactional
-    public boolean loginUser(String username, String password) {
-        ApplicationUser user = applicationUserRepository.findByUsername(username);
-        if (user.getPassword().equals(password)) {
-          user.setIsLoggedIn(true);
-          return true;
-          }        
-        return false;
+
+    if (customer == null)
+      throw new Exception("User must be a customer");
+
+    PaymentCredentials paymentCredentials = new PaymentCredentials();
+    paymentCredentials.setCardHolderName(cardHolderName);
+    paymentCredentials.setCcNumber(ccNumber);
+    paymentCredentials.setCvc(cvc);
+    paymentCredentials.setExpirationDate(expirationDate);
+
+    customer.getPaymentCredentials().add(paymentCredentials);
+
+    paymentCredentialsRepository.save(paymentCredentials);
+    customerRepository.save(customer);
+    applicationUserRepository.save(user);
+    return user;
+  }
+
+  /**
+   * Sets the role(s) of a user based on the role(s) inputed
+   * 
+   * @param username
+   * @param roles=
+   * @author ryadammar1
+   */
+  @Transactional
+  public ApplicationUser updateUserRole(String username, UserRole... roles) {
+    ApplicationUser user = applicationUserRepository.findByUsername(username);
+    HashSet<UserRole> roles_ = new HashSet<UserRole>();
+    for (UserRole role : roles)
+      roles_.add(role);
+
+    user.setUserRole(roles_);
+    applicationUserRepository.save(user);
+    return user;
+  }
+
+  @Transactional
+  public void logoutUser(String username) {
+    ApplicationUser user = applicationUserRepository.findByUsername(username);
+    user.setIsLoggedIn(false);
+
+    applicationUserRepository.save(user);
+  }
+
+  @Transactional
+  public boolean loginUser(String username, String password) {
+    ApplicationUser user = applicationUserRepository.findByUsername(username);
+    if (user.getPassword().equals(password)) {
+      user.setIsLoggedIn(true);
+      applicationUserRepository.save(user);
+      return true;
     }
-    
+    applicationUserRepository.save(user);
+    return false;
+  }
+
+
+
 }
