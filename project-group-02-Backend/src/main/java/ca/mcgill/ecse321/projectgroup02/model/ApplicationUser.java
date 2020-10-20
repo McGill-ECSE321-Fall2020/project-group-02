49dc55d186896cf6c3e6fe4cb3fedcce654f197c
ryadammar1 <mohamed.ammar@mail.mcgill.ca>
2020-10-20 15:19:37 -0400

    model user class anotation

--- project-group-02-Backend/src/main/java/ca/mcgill/ecse321/projectgroup02/model/ApplicationUser.java
+++ project-group-02-Backend/src/main/java/ca/mcgill/ecse321/projectgroup02/model/ApplicationUser.java
@@ -26,7 +26,7 @@ private String username;
 public void setUsername(String value) {
 this.username = value;
     }
-@Id
+
 public String getUsername() {
 return this.username;
     }
@@ -67,6 +67,7 @@ private int applicationUserId;
 public void setApplicationUserId(int value) {
 this.applicationUserId = value;
     }
+@Id
 public int getApplicationUserId() {
 return this.applicationUserId;
     }
