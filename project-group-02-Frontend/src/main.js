6b7332f0a7eed8076704cbd8fc74af6a6c5b24de
GurdarshanS <59708264+GurdarshanS@users.noreply.github.com>
2020-11-07 13:15:09 -0500

    Trying Login page

--- project-group-02-Frontend/src/main.js
+++ project-group-02-Frontend/src/main.js
@@ -6,6 +6,7 @@ import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/assets/css/main.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
@@ -16,4 +17,5 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
+
 })
