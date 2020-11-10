a591a27f53f83275e3a0803db43524e9d5bcaff1
GurdarshanS <59708264+GurdarshanS@users.noreply.github.com>
2020-11-09 21:22:39 -0500

    Adjusting Signup Page

---
+++ project-group-02-Frontend/src/router/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import Login from '@/components/Login/Login'
+import Signup from '@/components/Signup/Signup'
+import Item from "../components/Items/Item";
+import ItemsList from "../components/Items/ItemsList";
+import CollectionsList from "../components/Collections/CollectionsList";
+import Profile from "../components/Profile/Profile";
+import Edit from "../components/Edit/Edit";
+import Checkout from "../components/Checkout/Checkout";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+Vue.use(Router)
+
+let CollectionsPage;
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'signup',
+      component: Signup
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/collections',
+      name: 'collectionsList',
+      component: CollectionsList
+    },
+    {
+      path: '/collections/:collection',
+      name: 'itemsList',
+      component: ItemsList
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: Checkout
+    },
+    {
+      path: '/profile/:username',
+      name: 'profile',
+      component: Profile
+    },
+    {
+      path: '/edit',
+      name: 'edit',
+      component: Edit
+    },
+  ]
+})
