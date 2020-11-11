71099005e0ac4a9fc04b83e2a0e953de6bf1d7f4
Ahmad Siddiqi <ahmad.siddiqi158@gmail.com>
2020-11-11 09:01:40 -0500

    Connected create collection page to the backend

--- project-group-02-Frontend/src/components/CreateCollection/CreateCollection.vue
+++ project-group-02-Frontend/src/components/CreateCollection/CreateCollection.vue
@@ -8,11 +8,13 @@
     <form class="" action="/compose" method="post">
       <div class="form-group">
 
-        <label>Collection Name</label>
-        <input class="txtbox form-control" type="text" name="postTitle">
+        <label for="cNamInput">Collection Name</label>
+        <input v-model="c_name" id="cNameInput" class="txtbox form-control" type="text" name="C_name">
+        <p>{{c_name}} </p>
 
         <label>Collection Description</label>
-        <textarea class="txtbox form-control" name="postContent" rows="8"></textarea>
+        <textarea v-model="c_description" class="txtbox form-control" name="postContent" rows="8"></textarea>
+        <p>{{c_description}} </p>
 
         <div class= "">
         <label for="exampleInputFile">Upload Collection image</label>
@@ -21,11 +23,59 @@
 
       </div>
 
-      <button class=" btn btn-dark" type="submit" name="button">Publish</button>
+      <router-link to='/collections'>
+        <button @click="uploadCollection" class=" btn btn-dark" type="submit" name="button">Publish</button>
+      </router-link>
+        
+
+      <div v-if="collectionError">
+        <p>{{collectionError}}</p> 
+      </div>
     </form>
   </div>
 </body>
 </template>
+
+<script>
+
+import AXIOS from "../../App"
+
+export default {
+  name: "CreateCollection",
+  
+  data() {
+    return{
+      c_name: "",
+      c_description: "",
+      c_imageURL: "",
+      collection: "",
+      collectionError: ""
+    }
+  },
+
+  methods: {
+    uploadCollection: function() {
+      /* AXIOS.post('/create-collection/'.concat(this.c_name) + '?collectionDescription' .concat(this.c_description) + '&collectionImageUrl' .concat(this.c_imageURL))
+         .then(response => {
+          this.collection = response.data;
+        })
+        .catch(error => {
+          this.collectionError = error;
+        }) */
+    }
+  }
+
+  
+}
+  
+  
+
+</script>
+
+
+
+
+
 
 <style>
 .heading {
