14fefa55391bf2a7f8698f2e5de33284237b8e4a
VadimT7 <vadim.tuchila@mail.mcgill.ca>
2020-11-13 15:21:23 -0500

    Resolved merge conflict

--- project-group-02-Frontend/src/components/Profile/Profile.vue
+++ project-group-02-Frontend/src/components/Profile/Profile.vue
@@ -2,107 +2,53 @@
 </script>
 
 <template>
-    <div class="vue-template vertical-center-profile inner-block">
-        <form>
-           <br>
-            <h1 style="text-align:center">My Profile</h1>
-               <br>
+  <div class="vue-template vertical-center-profile inner-block">
+    <form>
+      <br>
+      <h1 style="text-align:center">My Profile</h1>
+      <br>
 
-            <div class="form-group">
-                <label>Username</label>
-                <input @click="findUsername" type="text" id="username" class="form-control form-control-lg" readonly />
-            </div>
+      <div class="form-group">
+        <label>Username</label>
+        <input @click="findUsername" type="text" id="username" class="form-control form-control-lg" readonly/>
+      </div>
 
-            <div class="form-group">
-                <label>Email address</label>
-                <input @click="findUserEmail" type="text" id="address" class="form-control form-control-lg" readonly />
-            </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input @click="findUserEmail" type="text" id="address" class="form-control form-control-lg" readonly/>
+      </div>
 
-            <div class="form-group">
-                <label>Password</label>
-                <input @click="findUserPassword" type="text" id="password" class="form-control form-control-lg" readonly />
-            </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input @click="findUserPassword" type="text" id="password" class="form-control form-control-lg" readonly/>
+      </div>
 
-            <div class="form-group">
-                <label>Address</label>
-                <input @click="findUserAddress" type="text" id="address" class="form-control form-control-lg" readonly />
-            </div>
+      <div class="form-group">
+        <label>Address</label>
+        <input @click="findUserAddress" type="text" id="address" class="form-control form-control-lg" readonly/>
+      </div>
 
-            <div class="form-group">
-                <label>Payment Credentials</label>
-                <input @click="findUserCredentials" type="text" id="payment" class="form-control form-control-lg" readonly />
-            </div>
+      <div class="form-group">
+        <label>Payment Credentials</label>
+        <input @click="findUserCredentials" type="text" id="payment" class="form-control form-control-lg" readonly/>
+      </div>
 
-            <router-link :to="{name: 'collectionsList'}">
-                <button type="submit" class="btn btn-dark btn-lg btn-block">Visit Our Catalog</button>
-            </router-link>
+      <router-link :to="{name: 'collectionsList'}">
+        <button type="submit" class="btn btn-dark btn-lg btn-block">Visit Our Catalog</button>
+      </router-link>
 
-            <p class="forgot-password text-right">
-                Edit your profile
-                <a href="/#/edit" >here</a>
+      <p class="forgot-password text-right">
+        Edit your profile
+        <a href="/#/edit">here</a>
 
-            </p>
+      </p>
 
-            <div v-if = "userError"> {{userError}} </div>
+      <div v-if="userError"> {{ userError }}</div>
 
-        </form>
-    </div>
+    </form>
+  </div>
 </template>
 
-
-<<<<<<< HEAD
-<script>
-    export default {
-        data() {
-            return {
-              user: "",
-              userError: ""
-            }
-        },
-      created: function () {
-        this.user = new ApplicationUserDTO()
-
-        this.AXIOS.get('/user-by-name/' .concat(this.$username))
-        .then(response => {
-          this.user = response.data;
-        })
-        .catch(error => {
-          this.userError = "There was a problem fetching the user information";
-        })
-      }
-    }
-</script>
-
-||||||| abff8c8
-<script>
-    export default {
-        data() {
-
-            return {
-              user: "",
-              userError: ""
-            }
-        },
-      created: function () {
-        this.user = new ApplicationUserDTO()
-        
-        AXIOS.get('/user-by-name/' .concat($username))
-        .then(response => {
-          this.user = response.data;
-        })
-        .catch(error => {
-          this.userError = "There was a problem fetching the user information";
-        }) 
-      }
-    }
-</script>
-
-
-
-=======
-
-
->>>>>>> c853c6cb4a88b761d688fc0b92bb3acaaa4bafa5
 <style>
 
 body {
@@ -150,6 +96,7 @@ html,
   transform: translate(-50%, -50%);
   height: 100%;
 }
+
 .vertical-center-edit {
   display: flex;
   text-align: left;
@@ -214,6 +161,7 @@ label {
   margin: 0;
   padding: 0;
 }
+
 .social-icons ul li {
   display: inline-block;
   zoom: 1;
@@ -233,6 +181,7 @@ label {
   margin: 0 5px;
   text-decoration: none;
 }
+
 .social-icons ul li a i {
   -webkit-transition: all 0.2s ease-in;
   -moz-transition: all 0.2s ease-in;
@@ -252,101 +201,78 @@ label {
 
 <script>
 export default {
-    name: 'Profile',
-    data() {
-        return {
-            user: '',
-            email: '',
-            username: '',
-            address: '',
-            password: '',
-            paymentCredentials: '',
-            error: '',
-            credentials: ''
-        }
-    },
-
-    created: function () {
-        this.AXIOS.get('/user-by-name' + '/'.concat($username))
-            .then(response => {
-                this.user = $user
-            })
-            .catch(e => {
-                this.error = e;
-            });
-    },
-
-    methods: {
-        findUsername: function () {
-<<<<<<< HEAD
-            this.AXIOS.get('/create-user' + '/'.concat($username))
-||||||| abff8c8
-            AXIOS.get('/create-user' + '/'.concat($username))
-=======
-            AXIOS.get('/user-by-name' + '/'.concat($username))
->>>>>>> c853c6cb4a88b761d688fc0b92bb3acaaa4bafa5
-                .then(response => {
-                    this.username = $username
-                })
-                .catch(e => {
-                    this.error = e;
-                });
-        },
-        findUserEmail: function () {
-<<<<<<< HEAD
-            this.AXIOS.get('/create-user' + '/'.concat($username) + '/email') // do /email??? or concat??
-||||||| abff8c8
-            AXIOS.get('/create-user' + '/'.concat($username) + '/email') // do /email??? or concat??
-=======
-            AXIOS.get('/user-by-name' + '/'.concat($username))
->>>>>>> c853c6cb4a88b761d688fc0b92bb3acaaa4bafa5
-                .then(response => {
-                    this.email = $user.email
-                })
-                .catch(e => {
-                    this.error = e;
-                });
-        },
-        findUserPassword: function () {
-<<<<<<< HEAD
-            this.AXIOS.get('/create-user' + '/'.concat($username) + '/email/pw')
-||||||| abff8c8
-            AXIOS.get('/create-user' + '/'.concat($username) + '/email/pw')
-=======
-            AXIOS.get('/user-by-name' + '/'.concat($username))
->>>>>>> c853c6cb4a88b761d688fc0b92bb3acaaa4bafa5
-                .then(response => {
-                    this.password = $user.password
-                })
-                .catch(e => {
-                    this.error = e;
-                });
-        },
-        findUserAddress: function () {
-<<<<<<< HEAD
-            this.AXIOS.get('/update-user-address' + '/'.concat($username) + '/street/postalcode/province/country/city')
-||||||| abff8c8
-            AXIOS.get('/update-user-address' + '/'.concat($username) + '/street/postalcode/province/country/city')
-=======
-            AXIOS.get('/user-by-name' + '/'.concat($username))
-                .then(response => {
-                    this.address = $user.address
-                })
-                .catch(e => {
-                    this.error = e;
-                });
-        },
-        findUserCredentials: function () {
-            AXIOS.get('/user-by-name' + '/'.concat($username))
->>>>>>> c853c6cb4a88b761d688fc0b92bb3acaaa4bafa5
-                .then(response => {
-                    this.credentials = $user.credentials
-                })
-                .catch(e => {
-                    this.error = e;
-                });
-        },
+  name: 'Profile',
+  data() {
+    return {
+      user: '',
+      email: '',
+      username: '',
+      address: '',
+      password: '',
+      paymentCredentials: '',
+      error: '',
+      credentials: '',
+      userError: ""
     }
+  },
+  created: function () {
+    this.user = new ApplicationUserDTO()
+
+    AXIOS.get('/user-by-name/'.concat($username))
+      .then(response => {
+        this.user = response.data;
+      })
+      .catch(error => {
+        this.userError = "There was a problem fetching the user information";
+      })
+  },
+  methods: {
+    findUsername: function () {
+      this.AXIOS.get('/user-by-name' + '/'.concat($username))
+        .then(response => {
+          this.username = $username
+        })
+        .catch(e => {
+          this.error = e;
+        });
+    },
+    findUserEmail: function () {
+      this.AXIOS.get('/user-by-name' + '/'.concat($username))
+        .then(response => {
+          this.email = $user.email
+        })
+        .catch(e => {
+          this.error = e;
+        });
+    },
+    findUserPassword: function () {
+      this.AXIOS.get('/user-by-name' + '/'.concat($username))
+        .then(response => {
+          this.password = $user.password
+        })
+        .catch(e => {
+          this.error = e;
+        });
+    },
+    findUserAddress: function () {
+      this.AXIOS.get('/user-by-name' + '/'.concat($username))
+        .then(response => {
+          this.address = $user.address
+        })
+        .catch(e => {
+          this.error = e;
+        });
+    },
+    findUserCredentials: function () {
+      this.AXIOS.get('/user-by-name' + '/'.concat($username))
+        .then(response => {
+          this.credentials = $user.credentials
+        })
+        .catch(e => {
+          this.error = e;
+        });
+    },
+  }
 }
 
 </script>
