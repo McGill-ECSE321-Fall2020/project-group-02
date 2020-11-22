3dcb62f15e969f3e82d86e2961d6bf1ebb28f47f
Ahmad Siddiqi <ahmad.siddiqi158@gmail.com>
2020-11-22 18:32:52 -0500

    Fixed Login page

--- project-group-02-Android/app/src/main/java/com/example/projectgroup02/LoginActivity.java
+++ project-group-02-Android/app/src/main/java/com/example/projectgroup02/LoginActivity.java
@@ -2,15 +2,17 @@ package com.example.projectgroup02;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
+import android.view.View;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,15 +20,23 @@ import org.json.JSONObject;
 import cz.msebera.android.httpclient.Header;
 
 public class LoginActivity extends AppCompatActivity {
-    private Toolbar toolbar;
     private String error = "";
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
 
-        toolbar=findViewById(R.id.GalleryHeader);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     public void login(View v) {
@@ -34,15 +44,15 @@ public class LoginActivity extends AppCompatActivity {
         final TextView tvPassword = (TextView) findViewById(R.id.editTextPassword);
 
         HttpUtils.post("user-login/" + tvUsername.getText().toString() + "/" + tvPassword.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
+
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 tvUsername.setText("");
                 tvPassword.setText("");
                 //MainActivity.username = response;
-                Intent intent = new Intent(v.getContext(), CollectionsPageActivity.class);
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
                 startActivity(intent);
             }
-
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 try {
                     error = errorResponse.get("message").toString();
@@ -52,5 +62,4 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
 }
