6a982b5922474568c742aa3e28a97d20be526b73
ryadammar1 <mohamed.ammar@mail.mcgill.ca>
2020-11-24 15:23:58 -0500

    checkout acitvity

--- project-group-02-Android/app/src/main/java/com/example/projectgroup02/CheckoutActivity.java
+++ project-group-02-Android/app/src/main/java/com/example/projectgroup02/CheckoutActivity.java
@@ -1,16 +1,31 @@
 package com.example.projectgroup02;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class CheckoutActivity extends AppCompatActivity {
+    public String deliveryMethod = "";
+    private String error = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +34,60 @@ public class CheckoutActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button checkout = (Button) findViewById(R.id.checkout);
+        checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                checkout();
+            }
+        });
+
+        Switch delivery = (Switch) findViewById(R.id.delivery);
+        delivery.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    deliveryMethod = "HOME";
+                } else {
+                    deliveryMethod = "STORE";
+                }
             }
         });
     }
+
+    private void checkout() {
+        try {
+            HttpUtils.post(MainActivity.username + "/checkout/" + deliveryMethod, new RequestParams(), new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    refreshErrorMessage();
+                    startActivity(new Intent(CheckoutActivity.this, BrowseCollectionsPageActivity.class));
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    try {
+                        error += errorResponse.get("message").toString();
+                    } catch (JSONException e) {
+                        error += e.getMessage();
+                    }
+                    refreshErrorMessage();
+                }
+            });
+        }catch (Exception e) {
+
+        }
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.errorcheckout);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
